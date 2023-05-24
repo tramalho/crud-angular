cb1b46a17ae2f63cea059abe6f37dc122c3f1995
tramalho <thiagofacul@gmail.com>
2023-05-23 23:12:31 -0300

    CRUD Angular + Spring | 08

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,3 +1,4 @@
+import { CoursesService } from './../services/courses.service';
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
 
@@ -8,8 +9,10 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent {
 
-  courses: Course[] = [
-    {_id: "1", name: "Angular", category: "Front-End"}
-  ];
+  courses: Course[] = [];
   displayedColumns = ['name', 'category']
+
+  constructor(private coursesService: CoursesService) {
+    this.courses = coursesService.list()
+  }
 }

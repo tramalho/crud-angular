ba82b359da5aff2c89dcdaea6367f70009da7f62
tramalho <thiagofacul@gmail.com>
2023-05-23 22:20:28 -0300

    CRUD Angular + Spring | 06

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class CoursesComponent {
 
+  courses: Course[] = [
+    {_id: "1", name: "Angular", category: "Front-End"}
+  ];
+  displayedColumns = ['name', 'category']
 }
